8e5a2362598b51ce50e6e0cd2aadd18af6a1d7b9
luanhaipeng <luanhaipeng@staff.beechat.io>
2018-11-22 17:51:59 +0800

    up

--- src/main/java/com/github/linushp/orm/DataAccess.java
+++ src/main/java/com/github/linushp/orm/DataAccess.java
@@ -79,7 +79,7 @@ public class DataAccess {
             LOGGER.info("update sql : " + sql);
             connection = sqlSession.getConnection();
 
-            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement = connection.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
 
             for (int i = 0; i < args.length; i++) {
                 preparedStatement.setObject(i + 1, args[i]);
